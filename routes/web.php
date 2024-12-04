38fb7442420bdce6863314e34eda3a2b830e6439
Jazmin-Arcelly-Reyes-Guzman1 <t512700220@unitru.edu.pe>
2024-12-03 22:20:30 -0500

    feat: contacto/login

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('usuario.index');
 });
 
 //contacto
-use App\Http\Controllers\ContactoController;
 
 Route::get('/contacto', [ContactoController::class, 'index'])->name('contacto');
 
 //login
-use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
