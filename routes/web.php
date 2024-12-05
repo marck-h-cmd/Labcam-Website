cfc2658f3fc1c3efabcd37bb90c648df8c7b1683
carlosgs05 <daniel18.dg95@gmail.com>
2024-12-05 00:21:54 -0500

    Acomode la ruta de mi pestaña home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('usuario.index');
-});
+})->name('home');
 
 Route::get('/direccion', function(){
     return view ('usuario.Organizacion.Direccion');
