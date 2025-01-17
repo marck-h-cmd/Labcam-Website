c8ecbc998b70ab9f7d90d19270f808b49571f733
carlosgs05 <daniel18.dg95@gmail.com>
2025-01-17 10:25:21 -0500

    cambios_main

---
+++ database/seeders/SlidersSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Slider;
+
+class SlidersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Slider::create([
+            'img1' => 'carrusel_01_Sr2JGGXa0P.jpg',
+            'img2' => 'carrusel_02_AyZkfXzhMj.jpg',
+            'img3' => 'carrusel_03_fiy7QpT32W.jpg',
+        ]);
+    }
+}
