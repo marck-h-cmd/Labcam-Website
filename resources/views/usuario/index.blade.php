50e2a0718e5b00ae11e8caa4f04a4dff1c02638d
Jazmin-Arcelly-Reyes-Guzman1 <t512700220@unitru.edu.pe>
2025-01-22 17:42:44 -0500

    Segundos Cambios de feat/investigation

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -127,9 +127,7 @@
                     <!-- Texto descriptivo -->
                     <div class="mb-6">
                         <div class="my-6 flex flex-col space-y-4">
-
                             {!! $topProyecto->descripcion !!}
-
                         </div>
                         <a href="{{ route('proyectos') }}">
                             <button
